a86f6a1021af763997e89d2bad85d7d191b2a0b2
Jeremie Lagarde <jer@printstacktrace.org>
2014-05-12 01:18:48 +0200

    Adding image for rhq-apache-plugin

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 cd rhq
 docker build -t jerr/rhq .
+
 cd ../rhq-agent-debug
 docker build -t jerr/rhq-agent-debug .
+
+cd ../rhq-plugin-apache-debug
+docker build -t jerr/rhq-plugin-apache-debug .
